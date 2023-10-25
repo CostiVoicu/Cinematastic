739ef960860a7825ed05d3de4c911d243b31ad9a
Constantin Voicu <constantin-gc.voicu@student.unitbv.ro>
2023-10-25 19:04:25 +0300

    Added database conection

--- Cinematastic/settings.py
+++ Cinematastic/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Cinematastic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Cinematastic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bs3tzikw05wq1vmpc00e',
+        'USER': 'ukaphl0tuayirkbt',
+        'PASSWORD': 'lLpx4OS7HbUSG8OcDHZW',
+        'HOST': 'bs3tzikw05wq1vmpc00e-mysql.services.clever-cloud.com',
+        'PORT': '3306'
     }
 }
 
@@ -118,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
