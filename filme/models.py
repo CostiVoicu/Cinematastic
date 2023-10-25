f3606e67c5f98ab702e10168d26574270115219d
Constantin Voicu <constantin-gc.voicu@student.unitbv.ro>
2023-10-25 18:40:34 +0300

    Added models

--- filme/models.py
+++ filme/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class film(models.Model):
+    nume = models.TextField(default=None)
+    descriere = models.TextField(default=None)
+    score = models.TextField(default=None)
+    regizor = models.TextField(default=None)
+    star = models.TextField(default=None)
+    storyline = models.TextField(default=None)
+    data = models.DateField(default=None)
+    durata = models.DurationField(default=None)
+    azi = models.BooleanField(default=False)
+    imagine = models.ImageField(upload_to='static/images')
+    id_gen = models.ForeignKey('gen', on_delete=models.CASCADE)
+    ora1 = models.TimeField(default=None)
+    ora2 = models.TimeField(default=None)
+    trailer_link = models.TextField(default=None)
+
+class gen(models.Model):
+    nume = models.TextField()
